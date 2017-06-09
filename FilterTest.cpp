c31a3e1a8c940868af80acd64786b3555958f145
Josborne-cc <Josborne-cc@conestogac.on.ca>
2017-06-08 23:42:11 -0400

    update

--- FilterTest.cpp
+++ FilterTest.cpp
@@ -1,9 +1,21 @@
+/*
+Program: Convulution of a DTMF filter to determine the tone
+*/
+
 #include <iostream>
+#include <vector>
+#include "1209coefs.h"
+
+
 using std::cout;
 using std::endl;
 
 int main()
 {
-	cout << "Welcome" << endl;
+	int i;
+
+	for(i = 0; i < BL; i++)
+		cout << B[i] << endl;
+	
 	return 0;
 }
--- makefile
+++ makefile
@@ -5,7 +5,7 @@ default: FilterTest
 FilterTest: FilterTest.o
 	g++ $(CFLAGS) -o FilterTest FilterTest.o
 
-FilterTest.o: FilterTest.cpp
+FilterTest.o: FilterTest.cpp 1209coefs.h tmwtypes.h
 	g++ $(CFLAGS) -c FilterTest.cpp
 
 clean:
