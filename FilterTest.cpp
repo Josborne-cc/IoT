289b6fe31c27684e886d6bd22f15572e3aa98c43
Josborne-cc <Josborne-cc@conestogac.on.ca>
2017-06-20 18:13:25 -0400

    update

--- FilterTest.cpp
+++ FilterTest.cpp
@@ -12,26 +12,36 @@ using std::cout;
 using std::endl;
 
 #define SignalLen 512
+#define FilterLen 513
 #define MAXLEN 1025
 
-void convolve(double *signal, double *result);
+void convolve(double *signal, double *filter, double *result);
 
 int main()
 {
 	int i;
-
-	double x1209[512];
-	double result[MAXLEN];
+	double sum = 0.0;
+	bool x697;
+	double signal[512];
+	double r697[MAXLEN];
+	double r770[MAXLEN];
+	double r852[MAXLEN];
+	double r941[MAXLEN];
+	double r1209[MAXLEN];
+	double r1336[MAXLEN];
+	double r1477[MAXLEN];
+	double r1633[MAXLEN];
 	std::ifstream input;
 	std::ofstream output;
 
+	// Gather the input
 	input.open("DTMF tones/1336.samples", std::fstream::in);
 
 	if (input.is_open())
 	{
 		for(i = 0; i < SignalLen; i++)
 		{
-			input >> x1209[i];
+			input >> signal[i];
 			//cout << i << ": " << x1209[i] << endl;
 		}
 	}
@@ -40,10 +50,19 @@ int main()
 
 	input.close();
 
+	// Complete the convultion
+	convolve(signal,f697,r697);
+	convolve(signal,f770,r770);
+	convolve(signal,f852,r852);
+	convolve(signal,f941,r941);
+	convolve(signal,f1209,r1209);
+	convolve(signal,f1336,r1336);
+	convolve(signal,f1477,r1477);
+	convolve(signal,f1633,r1633);
 
-	convolve(x1209,result);
-
-
+	// Check to see if frequency is present
+	for(i = 0; i < MAXLEN; i++)
+		sum += r1336[i];
 
 	
 	// write the results to a file
@@ -55,24 +74,24 @@ int main()
 	return 0;
 }
 
-// Do the convolution
-void convolve(double *signal, double *result)
+// Convolution Function
+void convolve(double *signal, double *filter, double *result)
 {
 	int k;
 	int n;
 	int kmax;
 	int kmin;
 	
-	  for (n = 0; n < SignalLen + BL - 1; n++)
-	  {
-	    result[n] = 0;
+	for (n = 0; n < SignalLen + FilterLen - 1; n++)
+	{
+		result[n] = 0;
 
-	    kmin = (n >= BL - 1) ? n - (BL - 1) : 0;
-	    kmax = (n < SignalLen - 1) ? n : SignalLen - 1;
+		kmin = (n >= FilterLen - 1) ? n - (FilterLen - 1) : 0;
+		kmax = (n < SignalLen - 1) ? n : SignalLen - 1;
 
-	    for (k = kmin; k <= kmax; k++)
-	    {
-	      result[n] += signal[k] * B[n - k];
-	    }
-	  }
+		for (k = kmin; k <= kmax; k++)
+		{
+			result[n] += signal[k] * filter[n - k];
+		}
+	}
 }
