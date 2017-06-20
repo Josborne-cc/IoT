7af8e911850483f50eb472728b82aa95b78467e9
Josborne-cc <Josborne-cc@conestogac.on.ca>
2017-06-20 15:48:33 -0400

    update

--- FilterTest.cpp
+++ FilterTest.cpp
@@ -12,24 +12,24 @@ using std::cout;
 using std::endl;
 
 #define SignalLen 512
+#define MAXLEN 2000
+
+void convolve(double *signal, double *result);
 
 int main()
 {
 	int i;
-	int k;
-	int n;
-	int kmax;
-	int kmin;
+
 	double x1209[512];
-	double result[2000];
+	double result[MAXLEN];
 	std::ifstream input;
 	std::ofstream output;
 
-	input.open("DTMF tones/1336.samples", std::fstream::in);
+	input.open("DTMF tones/1209.samples", std::fstream::in);
 
 	if (input.is_open())
 	{
-		for(i = 0; i < 512; i++)
+		for(i = 0; i < SignalLen; i++)
 		{
 			input >> x1209[i];
 			//cout << i << ": " << x1209[i] << endl;
@@ -41,8 +41,28 @@ int main()
 	input.close();
 
 
-	
+	convolve(x1209,result);
 
+
+
+	
+	// write the results to a file
+	output.open("c1209.txt");
+	for(i = 0; i < MAXLEN; i++)
+		output << result[i] << endl;
+	output.close();
+	
+	return 0;
+}
+
+// Do the convolution
+void convolve(double *signal, double *result)
+{
+	int k;
+	int n;
+	int kmax;
+	int kmin;
+	
 	  for (n = 0; n < SignalLen + BL - 1; n++)
 	  {
 	    result[n] = 0;
@@ -52,23 +72,7 @@ int main()
 
 	    for (k = kmin; k <= kmax; k++)
 	    {
-	      result[n] += x1209[k] * B[n - k];
+	      result[n] += signal[k] * B[n - k];
 	    }
 	  }
-	
-
-
-	  for (i = 0; i < SignalLen; i++)
-	  {
-	    printf("[%zu] = %f\n", i, x1209[i]);
-	  }
-	  printf("\n");
-
-	// write the results to a file
-	output.open("c1209.txt");
-	for(i = 0; i < 2000; i++)
-		output << result[i] << endl;
-	output.close();
-	
-	return 0;
 }
