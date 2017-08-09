c44b029d0b4207ce18b4c47cf164d632decbb7a1
Josborne-cc <Josborne-cc@conestogac.on.ca>
2017-08-08 20:35:46 -0400

    update

--- Client/c.cpp
+++ Client/c.cpp
@@ -5,27 +5,78 @@
 
 int main ( int argc, int argv[] )
 {
-  try
-    {
-
-      ClientSocket client_socket ( "142.156.193.84", 5000 );
-
-      std::string reply;
-
-      try
+	int fd;
+	int tone;
+	
+	try
 	{
-	  client_socket << "Test message.";
-	  client_socket >> reply;
+
+		ClientSocket client_socket ( "142.156.193.84", 5000 );
+
+		std::string reply;
+
+		try
+		{
+			//client_socket << "Test message.";
+			fd = open_pipe("/home/josh/IoT/myfifo");
+			read_pipe(fd, &tone);
+			client_socket << tone;
+			client_socket >> reply;
+		}
+		catch ( SocketException& ) {}
+
+		std::cout << "We received this response from the server:\n\"" << reply << "\"\n";;
+
 	}
-      catch ( SocketException& ) {}
+	catch ( SocketException& e )
+	{
+	  std::cout << "Exception was caught:" << e.description() << "\n";
+	}
 
-      std::cout << "We received this response from the server:\n\"" << reply << "\"\n";;
+	return 0;
+	}
 
-    }
-  catch ( SocketException& e )
-    {
-      std::cout << "Exception was caught:" << e.description() << "\n";
-    }
+/***************open_pipe()****************************************************
+Purpose: Open a pipe for communication
+Input:   char* pipe - name of pipe to open
+Output:  int fd - file descripter to referecnce pipe
+******************************************************************************/
+int open_pipe(char *pipe)
+{
+	int fd;
 
-  return 0;
+	fd = open(pipe, (O_RDWR));
+	if (fd == -1)
+	{
+		perror("Open Failed");
+		exit(1);
+	}
+
+	return fd;
+}
+
+/*************************write_pipe()*****************************************
+Purpose: Write a message to a pipe
+Input:   int fd - file descripter of pipe to be written to
+	 int *address - address of message to be written
+******************************************************************************/
+int write_pipe(int fd, int *address)
+{
+	if(write(fd, address, sizeof(int)) == -1)
+		perror("Write Failed");
+	return 0;
+}
+
+
+
+/*******************read_pipe()************************************************
+Purpose: Read data from the pipe
+Input:   int fd - file descriptor for the pipe
+	 int *address - address of the location where the data is to be written
+******************************************************************************/
+int read_pipe(int fd, int *address)
+{
+	if(read(fd, address, sizeof(int)) == -1)
+		perror("Read Failed");
+	return 0;
 }
